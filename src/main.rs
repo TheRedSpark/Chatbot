5ca00e07882223b09ae56b58f0002999aed6e0f1
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-27 18:29:11 +0200

    Tests(Schema) Hinzugefügt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true)
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_works_not() {
+        assert!(false);
+    }
+}
