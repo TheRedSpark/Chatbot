bcdaa32cbb55d67311851134f4651c27e9b365d4
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-27 19:19:41 +0200

    Funktionale Übertragung für Commonication

--- src/Chatclient/main.rs
+++ src/Chatclient/main.rs
@@ -1,8 +1,38 @@
-fn main() {
-    println!("Hello, Chatclient!");
+#![allow(unused)]
+
+// https://github.com/rust-lang/rustlings  Wichtig
+use std::net;
+use std::net::{TcpStream, AddrParseError};
+use std::io::{Empty, Error, Read, Write};
+//use std::simd::u8x16;
+use std::str;
+use std::{thread, time};
+use std::fmt::Error as OtherError;
+
+fn communication() {
+    use std::net::{Shutdown, TcpStream};
+    let mut buf = [0 as u8; 48];
+    let client_id = b"c0000001";
+    let echo_key = b"jejdkztb";
+    let data = b"D000000000000003";
+    let command = b"C000000000000001";
+    let mut stream = TcpStream::connect("127.0.0.1:80")
+        .expect("Couldn't connect to the server...");
+    println!("Connectet");
+    stream.write(echo_key).unwrap();
+    stream.write(client_id).unwrap();
+    stream.write(data).unwrap();
+    stream.write(command).unwrap();
+    stream.read(&mut buf).unwrap();
+    println!("Der Befehl lautet: {}", str::from_utf8(&buf).unwrap());
+    stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+    thread::sleep(time::Duration::from_millis(5000));
+    let s = "Hat geklappt";
 }
 
-
+fn main() {
+    communication();
+}
 
 
 #[cfg(test)]
