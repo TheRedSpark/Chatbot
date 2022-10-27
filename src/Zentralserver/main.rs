4c5cee5aa402814661b37fa02ba90f0c4dd2cae7
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-27 19:12:59 +0200

    Übertrag von Repo Server-Client-comm für ummbau für bot

--- src/Zentralserver/main.rs
+++ src/Zentralserver/main.rs
@@ -1,8 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use std::str;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buff = [0 as u8; 48];
+    stream.read(&mut buff).unwrap();
+    let echo_key = str::from_utf8(&buff[0..8]).unwrap();
+    let client_id: &str = str::from_utf8(&buff[8..16]).unwrap();
+    let data = str::from_utf8(&buff[16..32]).unwrap();
+    let command = str::from_utf8(&buff[32..48]).unwrap();
+    println!("Echo is :{}", echo_key);
+    println!("Client {} ist verbunden", client_id);
+    println!("Data is: {}", data);
+    println!("Command is: {}", command);
+    let data = handle_data(data.as_bytes());
+    let response: String = format!("{}{}{}{}", echo_key, client_id, data, command);
+    stream.write(response.as_ref()).unwrap();
 }
 
+fn handle_data(incomming_data: &[u8]) -> String {
+    let data: [u8; 16] = *match incomming_data {
+        b"D000000000000001" => b"D000000000000002",
+        &_ => b"Der000005555ror1"
+    };
+    let response = str::from_utf8(&data).unwrap().parse().unwrap();
+    return response;
+}
 
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:80")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
 
 
 #[cfg(test)]
