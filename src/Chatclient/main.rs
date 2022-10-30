1271da7492bfa4c40ebf888056703e57f5f1517c
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-30 20:31:25 +0100

    V0.1.0 Client nimmt nun Userinput an mit größerem Buffer und Kontolle für die Bufferlänge

--- src/Chatclient/main.rs
+++ src/Chatclient/main.rs
@@ -1,51 +1,53 @@
 #![allow(unused)]
+
 use std::net;
 use std::net::{TcpStream, AddrParseError};
 use std::io::{Empty, Error, Read, Write};
 use std::str;
 use std::{thread, time};
 use std::fmt::Error as OtherError;
+use std::io;
 
-
-fn communication() {
+fn communication() //-> io::Result<()>
+{
+    let stdin = io::stdin();
     let server_ip = "127.0.0.1:80";
     use std::net::{Shutdown, TcpStream};
-    let mut buffer = [0 as u8; 48];
-    let client_id = "00000001";
-    let empfanger_id = "00000002";
-    let message = "Hallo Gil i bims";
-    let key = "The komische key";
+    let mut buff_res = [0 as u8; 48];
+    let client_id = "90000001";
+    let empfanger_id = "90000002";
+    let mut message = String::new();
+    //let mut message = "So weiter gehts because the blub is on the way to the moon and back say the kid";
+
+    while true {
+        stdin.read_line(&mut message);
+        if message.len() > 240 {
+            continue;
+        } else { break; }
+    }
+
+
     let mut stream = TcpStream::connect(server_ip)
         .expect("Couldn't connect to the server...");
     println!("Connectet");
 
+    let buff_send = (client_id.to_owned() + empfanger_id + &message);
 
-    let mut buff_temp = [0 as u8; 48];
-    let hjadsg = "id";
-    let buff_temp2 = (client_id.to_owned()+empfanger_id+message+key);
-    println!("Der Client sendet: {}",buff_temp2);
-
-    stream.write(buff_temp2.as_ref());
-/*    stream.write(client_id).unwrap();
-    stream.write(empfanger_id).unwrap();
-    stream.write(message).unwrap();
-    stream.write(key).unwrap();*/
-    stream.read(&mut buffer).unwrap();
-    println!("Der Server sendet: {}", str::from_utf8(&buffer).unwrap());
-    //let response: String = format!("{:?}{}{}{}", str::from_utf8(client_id).unwrap(), &empfanger_id, &message, &key);
-    //let echo_key = str::from_utf8(&buff[0..8]).unwrap();
-    //stream.write(response.as_ref()).unwrap();
+    println!("Der Client sendet: {}", buff_send);
+    stream.write(buff_send.as_ref());
+    stream.read(&mut buff_res).unwrap();
+    println!("Der Server sendet: {}", str::from_utf8(&buff_res).unwrap());
+    if str::from_utf8(&buff_res).unwrap() != buff_send {
+        println!("Beim senden ist was schiefegangen")
+    }
 
 
     stream.shutdown(Shutdown::Both).expect("shutdown call failed");
-    //thread::sleep(time::Duration::from_millis(5000));
-    let s = "Hat geklappt";
-    println!("{}",s);
+    thread::sleep(time::Duration::from_millis(500));
 }
 
 fn main() {
     communication();
-
 }
 
 
