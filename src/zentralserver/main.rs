1167a0f3bc892750ef07a4cf699a1184ee36605c
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-30 14:29:27 +0100

    -

--- src/zentralserver/main.rs
+++ src/zentralserver/main.rs
@@ -5,24 +5,29 @@ use std::io::{Read, Write};
 use std::str;
 use mysql::*;
 use mysql::prelude::*;
+use crate::dataanbin::datenbank_putter;
 
 mod dataanbin;
 
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let mut buff = [0 as u8; 48];
     stream.read(&mut buff).unwrap();
-    let echo_key = str::from_utf8(&buff[0..8]).unwrap();
-    let client_id: &str = str::from_utf8(&buff[8..16]).unwrap();
-    let data = str::from_utf8(&buff[16..32]).unwrap();
-    let command = str::from_utf8(&buff[32..48]).unwrap();
-    println!("Echo is :{}", echo_key);
+    let sender_id_str = str::from_utf8(&buff[0..8]).unwrap();
+    let sender_id:i32= sender_id_str.parse().unwrap();
+    let client_id_str = str::from_utf8(&buff[8..16]).unwrap();
+    let client_id:i32 = client_id_str.parse().unwrap();
+    let message = str::from_utf8(&buff[16..32]).unwrap();
+    let key = str::from_utf8(&buff[32..48]).unwrap();
+    println!("Echo is :{}", sender_id);
     println!("Client {} ist verbunden", client_id);
-    println!("Data is: {}", data);
-    println!("Command is: {}", command);
+    println!("Data is: {}", message);
+    println!("Command is: {}", key);
     //let data = handle_data(data.as_bytes());
-    let response: String = format!("{}{}{}{}", echo_key, client_id, data, command);
+    let response: String = format!("{}{}{}{}", sender_id, client_id, message, key);
     stream.write(response.as_ref()).unwrap();
+    datenbank_putter(sender_id, client_id, message.to_string()).expect("TODO: panic message");
+    Ok(())
 }
 
 fn handle_data(incomming_data: &[u8]) -> String {
