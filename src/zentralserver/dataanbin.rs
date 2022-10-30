0b15159c04652e2891962db8cd74b0dc6410b5a2
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-30 14:26:54 +0100

    -

--- src/zentralserver/dataanbin.rs
+++ src/zentralserver/dataanbin.rs
@@ -15,7 +15,7 @@ pub(crate) fn string_builder() -> String {
     let mysql_user = variables::mysql_user("remote".to_string());
     let mysql_database = variables::mysql_database("test".to_string());
     let mysql_passwort = variables::mysql_passwort("remote".to_string());
-    println!("{}{}{}{}", mysql_database, mysql_user, mysql_ipaddr, mysql_passwort);
+    //println!("{}{}{}{}", mysql_database, mysql_user, mysql_ipaddr, mysql_passwort);
     let url: &str = &*format!("mysql://{mysql_user}:{mysql_passwort}@{mysql_ipaddr}:3306/{mysql_database}");
     return url.to_string();
 }
